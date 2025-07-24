299224b8d847c8ca87b5de15f2ccb04ed64f0143
ben <benbilldev@gmail.com>
2025-07-24 15:41:34 +0700

    chore: add cors middleware

--- app/main.py
+++ app/main.py
@@ -3,10 +3,21 @@ from app.scheduler import start_scheduler
 from app.routers import gold_prices
 from app.utils.logger import get_logger
 from app.database import Base, engine 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 logger = get_logger(__name__)
+#Middle ware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#App start up
 @app.on_event("startup")
 def startup_event():
     logger.info("🔧 Creating DB tables if not exists...")
